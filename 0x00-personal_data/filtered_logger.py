f4f551562638e61ffbc706b82cb71741ba07e4ab
husseinhassan92 <hussein.hassan2020@outlook.com>
2024-01-11 18:36:49 +0200

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,28 @@ from os import environ
 import mysql.connector
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Instantiation method, sets fields for each instance
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the LogRecord instance
+        """
+        log = super(RedactingFormatter, self).format(record=record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
+
+
 # # PII fields to be redacted
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
