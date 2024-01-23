6b1d89b9e92e73b6faeb2bad4ea4ff71999d040a
husseinhassan92 <hussein.hassan2020@outlook.com>
2024-01-23 23:09:16 +0200

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -22,15 +22,13 @@ def index() -> str:
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
     """POST /users
-    Create a new user.
     Return:
-        - The new user's payload.
+        - The account creation payload.
     """
-    email = request.form.get("email")
-    password = request.form.get("password")
+    email, password = request.form.get("email"), request.form.get("password")
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
@@ -47,4 +45,4 @@ def login() -> str:
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
-    return respons
+    return response
